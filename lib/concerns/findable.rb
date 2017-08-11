9f8596b3c994e107ceb9b8b66c3d34b43531114d
macro6461 <mattcroak718@gmail.com>
2017-08-11 15:16:32 -0400

    Done

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,9 @@
+module Findable
+
+  # attr_accessor :name
+
+ def find_by_name(name)
+     self.all.detect{|a| a.name == name}
+ end
+
+end
---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,9 @@
+module Paramable
+
+  attr_accessor :name
+
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
+
+end
